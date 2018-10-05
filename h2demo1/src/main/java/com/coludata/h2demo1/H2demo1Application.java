7858af3802f537df2086d5a5ccafe9b8bdda51a1
Thlo7777 <thlo7777@qq.com>
2018-10-05 20:45:43 +0800

    init commit

---
+++ h2demo1/src/main/java/com/coludata/h2demo1/H2demo1Application.java
@@ -0,0 +1,12 @@
+package com.coludata.h2demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2demo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2demo1Application.class, args);
+	}
+}
